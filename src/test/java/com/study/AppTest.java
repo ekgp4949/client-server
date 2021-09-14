34f51663a8e57c808cbc16e86b38bb9372f3f35b
ekgp4949 <ekgp4949@gmail.com>
2021-09-14 17:53:18 +0900

    [ADD] lesson handler 생성

--- src/test/java/com/study/AppTest.java
+++ src/test/java/com/study/AppTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-	
+
 	@DisplayName("App 테스트")
-    @Test
-    public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertThat(true).isTrue();
-        assertThat(classUnderTest).isNotNull();;
-    }
+	@Test
+	public void testAppHasAGreeting() {
+		App classUnderTest = new App();
+		assertThat(true).isTrue();
+		assertThat(classUnderTest).isNotNull();;
+	}
 }
