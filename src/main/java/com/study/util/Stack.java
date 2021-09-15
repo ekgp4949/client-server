b1428c28cd7a64a9fba611b133199b712e00e68c
ekgp4949 <ekgp4949@gmail.com>
2021-09-15 14:18:38 +0900

    Stack 구현

---
+++ src/main/java/com/study/util/Stack.java
@@ -0,0 +1,47 @@
+package com.study.util;
+
+import java.util.Arrays;
+
+public class Stack<E> implements Cloneable{
+	private static final int DEFUALT_SIZE = 10;
+
+	private Object[] list;
+	private int size = 0;
+
+	@Override
+	public Stack<E> clone() throws CloneNotSupportedException {
+		// 깊은 복사 해줘야함.
+		Stack<E> stack = new Stack<>();
+		for(int i = 0; i < size; i++) {
+			stack.push((E)list[i]);
+		}
+		return stack;
+	}
+
+	public Stack() {
+		list = new Object[DEFUALT_SIZE];
+	}
+
+	public void push(E e) {
+		if(size >= DEFUALT_SIZE) {
+			list = Arrays.copyOf(list, list.length + list.length >> 1);
+		}
+		list[size++] = e;
+	}
+
+	public E pop() {
+		if(size == 0) {
+			return null;
+		}
+		E element = (E)list[--size];
+		return element;
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+}
